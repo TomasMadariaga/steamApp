172e5607675b944f6391877f00b07c1237bfc107
TomasMadariaga <tomas.madariaga@municipiodetandil.edu.ar>
2024-02-04 22:15:27 -0300

    Se cambio la funcion handleAgreRestriction y se mejoro getDataFromGame para traer juego, bundle y compras por dropdown

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 import puppeteer from "puppeteer";
 
 async function handleAgeRestriction(p) {
-  await p.evaluate(() => {
-    const select = document.querySelector("#ageYear");
-    const options = select.querySelectorAll("option");
-    const selectedOption = [...options].find(
-      (option) => option.text === "1900"
+  try {
+    const ageGateSelectorExists = await p.$eval(
+      ".agegate_birthday_selector",
+      (el) => !!el
     );
 
-    selectedOption.selected = true;
-  });
-  await p.click("#view_product_page_btn");
+    if (ageGateSelectorExists) {
+      await p.evaluate(() => {
+        const select = document.querySelector("#ageYear");
+        const options = select.querySelectorAll("option");
+        const selectedOption = [...options].find(
+          (option) => option.text === "1900"
+        );
+
+        selectedOption.selected = true;
+      });
+    }
+
+    await p.click("#view_product_page_btn");
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 async function getDataFromGame() {
@@ -18,33 +30,49 @@ async function getDataFromGame() {
 
   const page = await browser.newPage();
 
-  await page.goto("https://store.steampowered.com/app/271590/");
+  const appId = 221100;
+
+  await page.goto(`https://store.steampowered.com/app/${appId}/`);
 
   await handleAgeRestriction(page);
 
-  await page.waitForSelector(".apphub_AppName");
-  // await page.waitForSelector(".game_purchase_price");
-  await page.waitForSelector("div.game_area_purchase_game > h1");
-  await page.waitForSelector(".discount_final_price");
+  await page.waitForSelector("div.game_area_purchase_game");
 
-  const result = await page.evaluate(() => {
-    const title = document.querySelector(".apphub_AppName").innerText;
-    // const price = document.querySelector(".game_purchase_price").innerText;
-    const bundle_title = document.querySelector(
-      "div.game_area_purchase_game > h1"
-    ).innerText;
-    const bundle_price = document.querySelector(
-      ".discount_final_price"
-    ).innerText;
+  const result = await page.$$eval(".game_area_purchase_game", (els) =>
+    els.map((bundle) => {
+      const bundle_title = bundle.querySelector("h1")?.textContent.trim();
+      const bundle_price = bundle
+        .querySelector(".game_purchase_price")
+        ?.textContent.trim();
+      const bundle_discount_price = bundle.querySelector(
+        ".discount_final_price"
+      )?.textContent;
 
-    const game = {
-      title,
-      // price,
-      bundle_title,
-      bundle_price,
-    };
-    return game;
-  });
+      if (bundle_price) {
+        return {
+          bundle_title,
+          bundle_price,
+        };
+      } else if (bundle_discount_price) {
+        return {
+          bundle_title,
+          bundle_discount_price,
+        };
+      }
+
+      bundle
+        .querySelector(".game_area_purchase_game_dropdown_selection")
+        ?.click();
+      return {
+        bundle_title,
+        bundle_prices: [
+          ...bundle.querySelectorAll(
+            ".game_area_purchase_game_dropdown_menu_item_text"
+          ),
+        ].map((e) => e.textContent),
+      };
+    })
+  );
 
   console.log(result);
 
